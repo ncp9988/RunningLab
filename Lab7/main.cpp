3d0671ca1b4b60d315fe22f05bd1167ad814d178
ncp9988 <ncp9988@gmail.com>
2022-10-24 09:58:01 -0500

    read in input file

--- Lab7/main.cpp
+++ Lab7/main.cpp
@@ -6,7 +6,19 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
 
 int main() {
+    ofstream myfile;
+     
+    myfile.open ("Result.txt");
     
+    ifstream infile;
+     
+     
+    infile.open("Result.txt");
+     
+   
 }
