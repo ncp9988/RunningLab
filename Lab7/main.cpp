d301b61cf2dafed5a382426cef5f66640fc0d784
ncp9988 <ncp9988@gmail.com>
2022-10-26 11:44:20 -0500

    output final time for first runner

--- Lab7/main.cpp
+++ Lab7/main.cpp
@@ -10,28 +10,8 @@
 #include <string>
 using namespace std;
 
-string readName ( string firstName, string lastName );
-int readTime ( int minute, int second );
-
-
-
-int main() {
-    ifstream infile;
-    infile.open("Result.txt");
-    string line;
-    getline(infile, line);
-    string name = readName(infile);
-    string team = readName(infile);
-    
-    
-
-    
-    return 0;
-}
-
 string readName ( ifstream&infile )
 {
-    int minute, second;
     string firstName;
     infile >> firstName;
     return firstName;
@@ -40,17 +20,38 @@ string readName ( ifstream&infile )
 int readTime (ifstream&infile)
 {
     int minute, second;
-    string header;
-    getline(infile, header);
-    string firsName, team;
-    getline (infile, firstName);
-    getline (infile, team);
     infile >> minute;
     infile.ignore(10, ':');
     infile>> second;
     return minute*60+second;
 
     
-
-    return 0;
 }
+
+void object1(int heat) {
+    ifstream infile;
+    infile.open("Result.txt");
+    string line;
+    if (heat==1)
+        getline(infile, line);
+    else ;
+    string name = readName(infile);
+    string team = readName(infile);
+    
+    int time1= readTime(infile);
+    int time2= readTime(infile);
+    int time3= readTime(infile);
+
+    cout << name << " "<< team <<" "<< (time1 + time2 + time3)<< " s" <<endl;
+    
+    //return 0;
+}
+
+int main ()
+{
+    object1(heat=1);
+}
+
+
+
+
