fece510d02d32fcce4ba020b7a55e90ef91ad425
ncp9988 <ncp9988@gmail.com>
2022-10-26 09:59:51 -0500

    trying to print out first and last name

--- Lab7/main.cpp
+++ Lab7/main.cpp
@@ -10,15 +10,26 @@
 #include <string>
 using namespace std;
 
+string readName ( string firstName, string lastName );
+
+
 int main() {
-    ofstream myfile;
-     
-    myfile.open ("Result.txt");
+    string firstName, lastName;
     
+    cout <<readName (firstName,lastName )<< endl;;
+
+
+    return 0;
+}
+
+string readName ( string firstName, string lastName )
+{
     ifstream infile;
-     
-     
-    infile.open("Result.txt");
-     
-   
+    infile.open ("Result.txt");
+    string header;
+    infile >> header;
+    getline(infile, header);
+    infile >> firstName >> lastName;
+    cout << firstName << lastName<< endl;
+    return firstName;
 }
