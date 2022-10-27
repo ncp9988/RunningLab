247debab481ca096f89e47a64f45a91210593963
ncp9988 <ncp9988@gmail.com>
2022-10-27 14:20:12 -0500

    output total time of each lap

--- Lab7/main.cpp
+++ Lab7/main.cpp
@@ -44,8 +44,10 @@ int readTime (ifstream&infile)
     
 }
 
-
 void object1(int heat) {
+    cout << "OBJECT 1:" << endl;
+    cout << " " << endl;
+
     ifstream infile;
     infile.open("Result.txt");
     string line;
@@ -64,7 +66,6 @@ void object1(int heat) {
         
         cout << "Heat 2:"<< endl;
     };
-    
     
     int place1time = 999;
     string place1 = "";
@@ -111,12 +112,71 @@ void object1(int heat) {
         }
     }
     cout << " "<< endl;
-
-    cout << "1st: " <<place1 << ", 2nd "<< place2 <<" 3rd "<< place3<<endl;
-    cout << " "<< endl;
-
-
+    cout << "1st: " <<place1 << ", 2nd: "<< place2 <<" 3rd: "<< place3<<endl;
+    cout << " ---------------------------------------------------------------"<< endl;
 }
+
+
+
+void object2(int heat) {
+    cout << "OBJECT 2:" << endl;
+    cout << " " << endl;
+
+    ifstream infile;
+    infile.open("Result.txt");
+    string line;
+    if (heat==1)
+    {
+        getline(infile, line);
+        cout << "Heat 1:"<< endl;
+        
+    }
+    else
+    {
+        getline(infile, line);
+        string runner;
+        for (int i =0; i <= totalRunner("Result.txt")+1 ; i++)
+            getline(infile, runner);
+        
+        cout << "Heat 2:"<< endl;
+    };
+    
+    int total1 = 0;
+    int total2 = 0;
+    int total3 = 0;
+
+    
+    for (int i =1; i <= totalRunner("Result.txt"); i++)
+    {
+        string name = readName(infile);
+        string team = readName(infile);
+        int lap1= readTime(infile);
+        int lap2= readTime(infile);
+        int lap3= readTime(infile);
+        
+    
+//        cout <<"lap1: " << name << " "<< team <<" "<< lap1<< " s" <<endl;
+//        cout <<"lap2: "<< name << " "<< team <<" "<< lap2<< " s" <<endl;
+//        cout <<"lap3: "<< name << " "<< team <<" "<< lap3<< " s" <<endl;
+        
+        if (1==1)
+        {
+            total1 =(total1+ lap1);
+            total2 =(total2+ lap2);
+            total3 =(total3+ lap3);
+
+        }
+       // cout << total1<< " " << total2 << " "<< total3 << endl;
+
+        
+    }
+    cout <<"Lap1:"<< total1/60 << ":"<< total1%60<< "s"<< endl;
+    cout <<"Lap2:"<< total2/60 << ":"<< total2%60<< "s"<<endl;
+    cout <<"Lap3:"<< total1/60 << ":"<< total3%60<< "s"<<endl;
+
+    cout << " ---------------------------------------------------------------"<< endl;
+}
+
 
 
 
@@ -126,11 +186,13 @@ void object1(int heat) {
 
 int main ()
 {
-    cout << totalRunner("Result.txt")<<endl;
+   // cout << totalRunner("Result.txt")<<endl;
     object1(1);
     object1(2);
 
-    //object2(1);
+    object2(1);
+    object2(2);
+
     }
 
 
